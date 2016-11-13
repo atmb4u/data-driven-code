498ba51576291b41fc1d37da05bb5f3b23069baa
Anoop Thomas Mathew <atmb4u@gmail.com>
2016-11-12 23:17:06 -0500

    minor modification in code for simplicity

--- demo.py
+++ demo.py
@@ -1,26 +1,26 @@
 from numpy import array, random, dot, exp
 
 inputs = array([[0, 0], [0, 1], [1, 0], [1, 1]])
-outputs = array([[0, 1, 1, 0]]).T
+outputs = array([[0], [1], [1], [0]])
 
-def learn(X,y):
-    layer1_weights = 2 * random.random((X.shape[1], 16)) - 1
-    layer2_weights = 2 * random.random((16, 1)) - 1
+def learn(X, y):
+    l1_w = 2 * random.random((X.shape[1], 16)) - 1
+    l2_w = 2 * random.random((16, 1)) - 1
     for j in xrange(10000):
-        layer1_estimation = 1/(1+exp(-(dot(X,layer1_weights))))
-        layer2_estimation = 1/(1+exp(-(dot(layer1_estimation,layer2_weights))))
-        layer2_estimation_delta = (y - layer2_estimation)*(layer2_estimation*(1-layer2_estimation))
-        layer1_estimation_delta = layer2_estimation_delta.dot(layer2_weights.T) * (layer1_estimation * (1-layer1_estimation))
-        layer2_weights += layer1_estimation.T.dot(layer2_estimation_delta)
-        layer1_weights += X.T.dot(layer1_estimation_delta)
-    return (layer1_weights, layer2_weights)
+        l1 = 1/(1+exp(-(dot(X,l1_w))))
+        l2 = 1/(1+exp(-(dot(l1,l2_w))))
+        l2_delta = (y - l2)*(l2*(1-l2))
+        l1_delta = l2_delta.dot(l2_w.T) * (l1 * (1-l1))
+        l2_w += l1.T.dot(l2_delta)
+        l1_w += X.T.dot(l1_delta)
+    return (l1_w, l2_w)
 
 xor_weights = learn(inputs, outputs)
 
 def predict(X, weights):
-    layer1_estimation = 1/(1+exp(-(dot(X, weights[0]))))
-    layer2_estimation = 1/(1+exp(-(dot(layer1_estimation, weights[1]))))
-    return layer2_estimation
+    l1 = 1/(1+exp(-(dot(X, weights[0]))))
+    l2 = 1/(1+exp(-(dot(l1, weights[1]))))
+    return l2
 
 test_set = [[0, 0], [0, 1], [1, 0], [1, 1]]
 for test_item in test_set:
